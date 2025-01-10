d9cc6434e47168eb8758de9bbee012ca5ad6aafa
David <david@speckle.systems>
2025-01-10 14:12:57 +0100

    modelcardid in double quotes

--- AddOns/Speckle/Sources/AddOn/Connector/Bridges/SendBridge.cpp
+++ AddOns/Speckle/Sources/AddOn/Connector/Bridges/SendBridge.cpp
@@ -117,7 +117,7 @@ void SendBridge::Send(const RunMethodEventArgs& args)
     }
     catch (const UserCancelledException&)
     {
-        std::string command = "triggerCancel(" + sendArgs.modelCardId + ")";
+        std::string command = "triggerCancel(\"" + sendArgs.modelCardId + "\")";
         args.eventSource->Emit(command);
     }
 
