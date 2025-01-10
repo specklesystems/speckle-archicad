4c7e510b1b40fcaf663aefbccb8fac3e37b29e49
David Kekesi <david@speckle.systems>
2025-01-10 10:36:39 +0100

    deleted build folder

--- build_27/AddOns/Speckle/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/speckle-archicad/speckle-archicad/AddOns/Speckle
-# Build directory: C:/dev/speckle-archicad/speckle-archicad/build_27/AddOns/Speckle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_27/AddOns/Speckle/ResourceObjects/ResourceMDIDIds.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef RESOURCEMDIDIDS_HPP
-#define RESOURCEMDIDIDS_HPP
-
-#define AC_MDID_DEV 
-#define AC_MDID_LOC 
-
-#endif
