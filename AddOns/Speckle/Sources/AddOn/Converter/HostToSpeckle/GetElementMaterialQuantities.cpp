fc895c494b0f41c215d160ae5619efde3f718d6f
David <david@speckle.systems>
2025-01-15 16:32:56 +0100

    wall quantities restructure

--- AddOns/Speckle/Sources/AddOn/Converter/HostToSpeckle/GetElementMaterialQuantities.cpp
+++ AddOns/Speckle/Sources/AddOn/Converter/HostToSpeckle/GetElementMaterialQuantities.cpp
@@ -8,6 +8,17 @@
 
 namespace
 {
+	std::string GetMaterialName(API_AttributeIndex materialId)
+	{
+		API_Attribute attr;
+		BNZeroMemory(&attr, sizeof(attr));
+		attr.header.typeID = API_MaterialID;
+		attr.header.index = materialId;
+		ACAPI_Attribute_Get(&attr);
+
+		return attr.header.name;
+	}
+
 	std::string GetBuildingMaterialName(API_AttributeIndex materialId)
 	{
 		API_Attribute attr;
@@ -28,6 +39,21 @@ namespace
 		ACAPI_Attribute_Get(&attr);
 
 		return attr.header.name;
+	}
+
+	std::string GetWorkingUnits()
+	{
+		API_WorkingUnitPrefs prefs{};
+		ACAPI_ProjectSetting_GetPreferences(&prefs, APIPrefs_WorkingUnitsID);
+
+		if (prefs.lengthUnit == API_LengthTypeID::Meter)
+		{
+			return "m";
+		}
+		else
+		{
+			return "m";
+		}
 	}
 
 	API_ElementQuantity GetElementQuantity(const API_Guid apiGuid)
@@ -53,17 +79,48 @@ namespace
 		auto elementQuantity = GetElementQuantity(apiElem.header.guid);
 		nlohmann::json quantities;
 
-		quantities["area"] = elementQuantity.wall.surface1;
-		quantities["volume"] = elementQuantity.wall.volume;
+		std::string materialName = "";
 
 		if (apiElem.wall.modelElemStructureType == API_BasicStructure)
 		{
-			quantities["materialName"] = GetBuildingMaterialName(apiElem.wall.buildingMaterial);
-			
+			materialName = GetBuildingMaterialName(apiElem.wall.buildingMaterial);
+
 		}
 		else if (apiElem.wall.modelElemStructureType == API_CompositeStructure)
 		{
-			quantities["materialName"] = GetCompositeMaterialName(apiElem.wall.composite);
+			materialName = GetCompositeMaterialName(apiElem.wall.composite);
+		}
+
+		if (!materialName.empty())
+		{
+			quantities[materialName]["materialName"] = materialName;
+			quantities[materialName]["volume"] = elementQuantity.wall.volume;
+			quantities[materialName]["area"] = elementQuantity.wall.area;
+			quantities[materialName]["units"] = "m";
+		}
+
+		if (apiElem.wall.sidMat.hasValue)
+		{
+			std::string sideMatName = GetMaterialName(apiElem.wall.sidMat.value);
+			quantities[sideMatName]["materialName"] = sideMatName;
+			quantities[sideMatName]["area"] = elementQuantity.wall.surface3;
+			quantities[sideMatName]["units"] = "m";
+		}
+
+		if (apiElem.wall.refMat.hasValue)
+		{
+			std::string refMatName = GetMaterialName(apiElem.wall.refMat.value);
+			quantities[refMatName]["materialName"] = refMatName;
+			quantities[refMatName]["area"] = elementQuantity.wall.surface1;
+			quantities[refMatName]["units"] = "m";
+		}
+
+		if (apiElem.wall.oppMat.hasValue)
+		{
+			std::string oppMatName = GetMaterialName(apiElem.wall.oppMat.value);
+			quantities[oppMatName]["materialName"] = oppMatName;
+			quantities[oppMatName]["area"] = elementQuantity.wall.surface2;
+			quantities[oppMatName]["units"] = "m";
 		}
 
 		return quantities;
@@ -90,23 +147,26 @@ namespace
 		return quantities;
 	}
 
+	// TODO: change this when we export beams as segmented elements
 	nlohmann::json GetBeamQuantity(const API_Element& apiElem)
 	{
 		auto elementQuantity = GetElementQuantity(apiElem.header.guid);
 		nlohmann::json quantities;
 
+		//quantities["area"] = (elementQuantity.beam.bottomSurface + elementQuantity.beam.topSurface + elementQuantity.beam.edgeSurface);
 		quantities["area"] = elementQuantity.beam.area;
 		quantities["volume"] = elementQuantity.beam.volume;
 
 		return quantities;
 	}
 
+	// TODO: change this when we export columns as segmented elements
 	nlohmann::json GetColumnQuantity(const API_Element& apiElem)
 	{
 		auto elementQuantity = GetElementQuantity(apiElem.header.guid);
 		nlohmann::json quantities;
 
-		quantities["area"] = elementQuantity.column.area;
+		quantities["area"] = elementQuantity.column.coreSurface;
 		quantities["volume"] = (elementQuantity.column.coreGrossVolume + elementQuantity.column.veneGrossVolume);
 
 		return quantities;
@@ -117,7 +177,7 @@ namespace
 		auto elementQuantity = GetElementQuantity(apiElem.header.guid);
 		nlohmann::json quantities;
 
-		quantities["area"] = elementQuantity.roof.bottomSurface;
+		quantities["area"] = elementQuantity.roof.contourArea;
 		quantities["volume"] = elementQuantity.roof.volume;
 
 		return quantities;
@@ -131,38 +191,28 @@ namespace
 		quantities["area"] = elementQuantity.shell.floorplanArea;
 		quantities["volume"] = elementQuantity.shell.volume;
 
-		return quantities;
-	}
+		if (apiElem.shell.shellBase.modelElemStructureType == API_BasicStructure)
+		{
+			quantities["materialName"] = GetBuildingMaterialName(apiElem.shell.shellBase.buildingMaterial);
 
-	nlohmann::json GetDoorQuantity(const API_Element& apiElem)
-	{
-		auto elementQuantity = GetElementQuantity(apiElem.header.guid);
-		nlohmann::json quantities;
-
-		quantities["area"] = elementQuantity.door.surface;
-		quantities["volume"] = elementQuantity.door.volume;
-
-		return quantities;
-	}
-
-	nlohmann::json GetWindowQuantity(const API_Element& apiElem)
-	{
-		auto elementQuantity = GetElementQuantity(apiElem.header.guid);
-		nlohmann::json quantities;
-
-		quantities["area"] = elementQuantity.window.surface;
-		quantities["volume"] = elementQuantity.window.volume;
+		}
+		else if (apiElem.shell.shellBase.modelElemStructureType == API_CompositeStructure)
+		{
+			quantities["materialName"] = GetCompositeMaterialName(apiElem.shell.shellBase.composite);
+		}
 
 		return quantities;
 	}
 
-	nlohmann::json GetStairQuantity(const API_Element& apiElem)
+	nlohmann::json GetMorphQuantity(const API_Element& apiElem)
 	{
 		auto elementQuantity = GetElementQuantity(apiElem.header.guid);
 		nlohmann::json quantities;
 
-		quantities["area"] = elementQuantity.stair.area;
-		quantities["volume"] = elementQuantity.stair.volume;
+		quantities["area"] = elementQuantity.morph.floorPlanArea;
+		quantities["volume"] = elementQuantity.morph.volume;
+
+		quantities["materialName"] = GetBuildingMaterialName(apiElem.morph.buildingMaterial);
 
 		return quantities;
 	}
@@ -186,12 +236,8 @@ nlohmann::json HostToSpeckleConverter::GetElementMaterialQuantities(const std::s
 		return GetRoofQuantity(apiElem);
 	case API_ShellID:
 		return GetShellQuantity(apiElem);
-	case API_DoorID:
-		return GetDoorQuantity(apiElem);
-	case API_WindowID:
-		return GetWindowQuantity(apiElem);
-	case API_StairID:
-		return GetStairQuantity(apiElem);
+	case API_MorphID:
+		return GetMorphQuantity(apiElem);
 
 	default:
 		return {};
