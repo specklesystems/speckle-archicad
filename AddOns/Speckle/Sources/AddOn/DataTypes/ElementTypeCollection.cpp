fb184a1b08dae58d794660cc10f0b7234c88f411
David <david@speckle.systems>
2025-01-31 13:41:46 +0100

    detaching bugfix

--- AddOns/Speckle/Sources/AddOn/DataTypes/ElementTypeCollection.cpp
+++ AddOns/Speckle/Sources/AddOn/DataTypes/ElementTypeCollection.cpp
@@ -4,5 +4,5 @@ void to_json(nlohmann::json& j, const ElementTypeCollection& elementType)
 {
     j["speckle_type"] = elementType.speckle_type;
     j["name"] = elementType.name;
-    j["elements"] = elementType.elements;
+    j["@elements"] = elementType.elements;
 }
