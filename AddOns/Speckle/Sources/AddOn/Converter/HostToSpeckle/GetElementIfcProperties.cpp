d0be103413729fc414d5bea3767f918b6da9f4c3
David Kekesi <david@speckle.systems>
2025-12-09 10:18:13 +0100

    ifc property access for AC 29

--- AddOns/Speckle/Sources/AddOn/Converter/HostToSpeckle/GetElementIfcProperties.cpp
+++ AddOns/Speckle/Sources/AddOn/Converter/HostToSpeckle/GetElementIfcProperties.cpp
@@ -3,11 +3,14 @@
 #include "HostToSpeckleConverter.h"
 #include "APIEnvir.h"
 #include "ACAPinc.h"
+#include "ACAPI/IFCObjectAccessor.hpp"
+#include "ACAPI/IFCObjectID.hpp"
+#include "ACAPI/IFCProperty.hpp"
+#include "ACAPI/IFCPropertyAccessor.hpp"
 #include "ConverterUtils.h"
 #include "CheckError.h"
 #include "SpeckleConversionException.h"
 
-/*
 namespace
 {
 	std::string bool_to_string(bool b)
@@ -30,35 +33,44 @@ namespace
 		}
 	}
 
-	std::string GetIfcPropertyValue(const API_IFCPropertyValue& propertyValue)
+	std::string GetIfcPropertyValue(const IFCAPI::Value& propertyValue)
 	{
-		switch (propertyValue.value.primitiveType)
+		auto anyValue = propertyValue.GetAnyValue();
+		if (anyValue.has_value())
 		{
-		case API_IFCPropertyAnyValueRealType:
-			return std::to_string(propertyValue.value.doubleValue);
-		case API_IFCPropertyAnyValueIntegerType:
-			return std::to_string(propertyValue.value.intValue);
-		case API_IFCPropertyAnyValueStringType:
-			return propertyValue.value.stringValue.ToCStr().Get();
-		case API_IFCPropertyAnyValueBooleanType:
-			return bool_to_string(propertyValue.value.boolValue);
-		case API_IFCPropertyAnyValueLogicalType:
-			return ifc_logical_to_string(static_cast<int>(propertyValue.value.intValue));
-
-		default:
-			throw SpeckleConversionException("Invalid IFC property value type.");
+			auto value = anyValue.value();
+			if (std::holds_alternative<GS::UniString>(value))
+			{
+				return std::get<GS::UniString>(value).ToCStr().Get();
+			}
+			else if (std::holds_alternative<bool>(value))
+			{
+				return bool_to_string(std::get<bool>(value));
+			}
+			else if (std::holds_alternative<double>(value))
+			{
+				return std::to_string(std::get<double>(value));
+			}
+			else if (std::holds_alternative<Int64>(value))
+			{
+				return std::to_string(std::get<Int64>(value));
+			}
+			else if (std::holds_alternative<IFCAPI::IfcLogical>(value))
+			{
+				return ifc_logical_to_string(static_cast<int>(std::get<IFCAPI::IfcLogical>(value)));
+			}
 		}
 	}
 
-	std::string GetIfcSinglePropertyValue(const API_IFCProperty& property)
+	std::string GetIfcSinglePropertyValue(const IFCAPI::PropertySingleValue& propertySingleValue)
 	{
-		return GetIfcPropertyValue(property.singleValue.nominalValue);
+		return GetIfcPropertyValue(propertySingleValue.GetNominalValue());
 	}
 
-	std::vector<std::string> GetIfcListPropertyValue(const API_IFCProperty& property)
+	std::vector<std::string> GetIfcListPropertyValue(const IFCAPI::PropertyListValue& propertyListValue)
 	{
 		std::vector<std::string> values;
-		for (const auto& val : property.listValue.listValues)
+		for (const auto& val : propertyListValue.GetListValues())
 		{
 			values.push_back(GetIfcPropertyValue(val));
 		}
@@ -66,19 +78,19 @@ namespace
 		return values;
 	}
 
-	std::pair<std::string, std::string> GetIfcBoundedPropertyValue(const API_IFCProperty& property)
+	std::pair<std::string, std::string> GetIfcBoundedPropertyValue(const IFCAPI::PropertyBoundedValue& propertyBoundedValue)
 	{
 		std::pair<std::string, std::string> values;
-		values.first = GetIfcPropertyValue(property.boundedValue.lowerBoundValue);
-		values.second = GetIfcPropertyValue(property.boundedValue.upperBoundValue);
+		values.first = GetIfcPropertyValue(propertyBoundedValue.GetLowerBoundValue());
+		values.second = GetIfcPropertyValue(propertyBoundedValue.GetUpperBoundValue());
 
 		return values;
 	}
 
-	std::vector<std::string> GetIfcEnumeratedPropertyValue(const API_IFCProperty& property)
+	std::vector<std::string> GetIfcEnumeratedPropertyValue(const IFCAPI::PropertyEnumeratedValue& propertyEnumeratedValue)
 	{
 		std::vector<std::string> values;
-		for (const auto& val : property.enumeratedValue.enumerationValues)
+		for (const auto& val : propertyEnumeratedValue.GetEnumerationValues())
 		{
 			values.push_back(GetIfcPropertyValue(val));
 		}
@@ -86,18 +98,18 @@ namespace
 		return values;
 	}
 
-	std::map<std::string, std::string> GetIfcTablePropertyValue(const API_IFCProperty& property)
+	std::map<std::string, std::string> GetIfcTablePropertyValue(const IFCAPI::PropertyTableValue& propertyTableValue)
 	{
 		std::map<std::string, std::string> values;
 
 		std::vector<std::string> definingValues;
-		for (const auto& val : property.tableValue.definingValues)
+		for (const auto& val : propertyTableValue.GetDefiningValues())
 		{
 			definingValues.push_back(GetIfcPropertyValue(val));
 		}
 
 		std::vector<std::string> definedValues;
-		for (const auto& val : property.tableValue.definedValues)
+		for (const auto& val : propertyTableValue.GetDefinedValues())
 		{
 			definedValues.push_back(GetIfcPropertyValue(val));
 		}
@@ -111,49 +123,58 @@ namespace
 		return values;
 	}
 	
-	nlohmann::json GetElementIfcPropertyValueAsJson(const API_IFCProperty& property)
+	nlohmann::json GetElementIfcPropertyValueAsJson(const IFCAPI::Property& property)
 	{
-		switch (property.head.propertyType)
+		auto propertyVariant = property.GetTyped();
+		if (std::holds_alternative<IFCAPI::PropertySingleValue>(propertyVariant)) 
 		{
-		case API_IFCPropertySingleValueType:
-			return GetIfcSinglePropertyValue(property);
-		case API_IFCPropertyListValueType:
-			return GetIfcListPropertyValue(property);
-		case API_IFCPropertyBoundedValueType:
-			return GetIfcBoundedPropertyValue(property);
-		case API_IFCPropertyEnumeratedValueType:
-			return GetIfcEnumeratedPropertyValue(property);
-		case API_IFCPropertyTableValueType:
-			return GetIfcTablePropertyValue(property);
-
-		default:
-			throw SpeckleConversionException("Invalid IFC property type.");
+			return GetIfcSinglePropertyValue(std::get<IFCAPI::PropertySingleValue>(propertyVariant));
+		}
+		else if (std::holds_alternative<IFCAPI::PropertyListValue>(propertyVariant)) 
+		{
+			return GetIfcListPropertyValue(std::get<IFCAPI::PropertyListValue>(propertyVariant));
+		}
+		else if (std::holds_alternative<IFCAPI::PropertyBoundedValue>(propertyVariant)) 
+		{
+			return GetIfcBoundedPropertyValue(std::get<IFCAPI::PropertyBoundedValue>(propertyVariant));
+		}
+		else if (std::holds_alternative<IFCAPI::PropertyEnumeratedValue>(propertyVariant)) 
+		{
+			return GetIfcEnumeratedPropertyValue(std::get<IFCAPI::PropertyEnumeratedValue>(propertyVariant));
+		}
+		else if (std::holds_alternative<IFCAPI::PropertyTableValue>(propertyVariant))
+		{
+			return GetIfcTablePropertyValue(std::get<IFCAPI::PropertyTableValue>(propertyVariant));
+		}
+		else
+		{
+			// TODO throw
 		}
 	}
 }
-*/
+
 nlohmann::json HostToSpeckleConverter::GetElementIfcProperties(const std::string& elemId)
 {
-	/*nlohmann::json properties;
+	nlohmann::json properties;
 	auto apiGuid = APIGuidFromString(elemId.c_str());
-	GS::Array<API_IFCProperty> ifcProperties;
-	GSErrCode err = NoError;
-	err = ACAPI_Element_GetIFCProperties(apiGuid, false, &ifcProperties);
-
-	if (err != NoError)
+	API_Elem_Head head{};
+	head.guid = apiGuid;
+	auto objectID = IFCAPI::GetObjectAccessor().CreateElementObjectID(head);
+	if (objectID.IsOk())
 	{
-		return {};
+		auto localProperties = IFCAPI::PropertyAccessor(objectID.Unwrap()).GetLocalProperties();
+		if (localProperties.IsOk())
+		{
+			for (const auto& property : localProperties.Unwrap())
+			{
+				std::string propertyName = property.GetName().ToCStr().Get();
+				std::string propertySetName = property.GetPropertySetName().ToCStr().Get();
+				nlohmann::json propertyValue = GetElementIfcPropertyValueAsJson(property);
+
+				properties[propertySetName][propertyName] = propertyValue;
+			}
+		}
 	}
 
-	for (const auto& property : ifcProperties)
-	{
-		std::string propertyName = property.head.propertyName.ToCStr().Get();
-		std::string propertySetName = property.head.propertySetName.ToCStr().Get();
-		nlohmann::json propertyValue = GetElementIfcPropertyValueAsJson(property);
-
-		properties[propertySetName][propertyName] = propertyValue;
-	}
-
-	return properties;*/
-	return {};
+	return properties;
 }
